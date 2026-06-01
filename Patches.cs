cf00be39a319e03837c759466041129ab7417bc6
NANDbrew <zortman@gmail.com>
2026-06-01 01:24:14 -0500

    added aestrin and emerald stoves, emerald smoker, aestrin stove, aestrin beds. Added backrest collider to aestrin chairs

--- Patches.cs
+++ Patches.cs
@@ -12,7 +12,7 @@ namespace BetterTables
             {
                 if (AssetTools.bundle == null) return;
 
-                int[] indices = { 64, 65, 66, 67, 68, 69, 72, 73, 74, 75, 76, 77, 78, 107, 377, 379 };
+                int[] indices = { 62, 63, 64, 65, 66, 67, 68, 69, 72, 73, 74, 75, 76, 77, 78, 105, 106, 107, 375, 377, 378, 379 };
 
                 for (int i = 0; i < indices.Length; i++)
                 {
@@ -23,15 +23,34 @@ namespace BetterTables
                         var bs = newThing.GetComponentsInChildren<Transform>();
                         foreach (Transform cc in bs)
                         {
+                            //cc.gameObject.tag = "ItemSubcollider";
                             cc.SetParent(oldThing.transform, false);
                             cc.transform.localEulerAngles = Vector3.zero;
                         }
                         oldThing.size = newThing.size;
                         oldThing.center = newThing.center;
+                        if (oldThing.GetComponent<MeshCollider>() is MeshCollider oldCol && newThing.GetComponent<MeshCollider>() is MeshCollider newCol)
+                        {
+                            oldCol.sharedMesh = newCol.sharedMesh;
+                        }
                         GameObject.Destroy(newThing.gameObject);
+
+                        // special handling for emerald smoker
+                        if (indices[i] == 378)
+                        {
+                            Transform[] children = oldThing.GetComponentsInChildren<Transform>();
+                            for (int t = 0; t < children.Length; t++)
+                            {
+                                if (children[t].name.StartsWith("col") && Mathf.Approximately(children[t].localPosition.y, 0))
+                                {
+                                    Debug.Log("destroying child: " + children[t].name);
+                                    GameObject.Destroy(children[t].gameObject);
+                                }
+                            }
+                        }
                         Debug.Log("BT: adjusted item: " + oldThing.name);
                     }
-                    catch { Debug.LogError("BetterTables: couldn't load asset for item " + indices[i]); }
+                    catch { Debug.LogError("BetterTables: failed to patch item " + indices[i]); }
                 }
             }
         }
